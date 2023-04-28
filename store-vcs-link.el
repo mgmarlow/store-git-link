4b3692adecd6615d89968bcd98084a86a116b1b5
mgmarlow <graham@mgmarlow.com>
2023-04-27 19:08:26 -0700

    init
    M-x auto-insert

---
+++ store-vcs-link.el
@@ -0,0 +1,29 @@
+;;; store-vcs-link.el --- Stores a web link to VCS repository at current point  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Graham Marlow
+
+;; Author: Graham Marlow <info@mgmarlow.com>
+;; Keywords: vc, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(provide 'store-vcs-link)
+;;; store-vcs-link.el ends here
